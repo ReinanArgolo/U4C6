693ef94334367fd84f4b8a85f376508d3ad6fff1
Reinan <reinanlopes.1@gmail.com>
2025-02-08 00:02:10 -0300

    Add WS2818B PIO program and configure I2C pins in U4C6

--- U4C6.c
+++ U4C6.c
@@ -1,39 +1,43 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "hardware/gpio.h"
+#include "hardware/i2c.h"
 
 #define LED_BLUE_PIN 11
 #define LED_RED_PIN 12
 #define LED_GREEN_PIN 13
+#define LED_MATRIZ_PIN 7
+#define B1_PIN 5
+#define B2_PIN 6
+
+// I2C Pins TELA LCD
+#define LCD_SDA_PIN 14
+#define LCD_SCL_PIN 15
 
 
 int main()
 {
     stdio_init_all();
-
     gpio_init(LED_BLUE_PIN);
-    gpio_set_dir(LED_BLUE_PIN, GPIO_OUT);
-
     gpio_init(LED_RED_PIN);
-    gpio_set_dir(LED_RED_PIN, GPIO_OUT);
-
     gpio_init(LED_GREEN_PIN);
+    gpio_init(LED_MATRIZ_PIN);
+    gpio_init(B1_PIN);
+    gpio_init(B2_PIN);
+    gpio_set_dir(LED_BLUE_PIN, GPIO_OUT);
+    gpio_set_dir(LED_RED_PIN, GPIO_OUT);
     gpio_set_dir(LED_GREEN_PIN, GPIO_OUT);
+    gpio_set_dir(LED_MATRIZ_PIN, GPIO_OUT);
+    gpio_set_dir(B1_PIN, GPIO_IN);
+    gpio_set_dir(B2_PIN, GPIO_IN);
+
+    
+
+
+   
 
     while (true) {
-        gpio_put(LED_BLUE_PIN, true);
-        sleep_ms(500);
-        gpio_put(LED_BLUE_PIN, 0);
-        sleep_ms(500);
-
-        gpio_put(LED_RED_PIN, 1);
-        sleep_ms(500);
-        gpio_put(LED_RED_PIN, 0);
-        sleep_ms(500);
-
-        gpio_put(LED_GREEN_PIN, 1);
-        sleep_ms(500);
-        gpio_put(LED_GREEN_PIN, 0);
-        sleep_ms(500);
+       
         
     }
 }
